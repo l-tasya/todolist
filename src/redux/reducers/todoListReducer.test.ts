ee74171ba2f3d46683030e3cd5a9ffb8b8d9563d
l-tasya <94777517+l-tasya@users.noreply.github.com>
2022-12-27 21:40:55 +0600

    add TODO test

--- src/redux/reducers/todoListReducer.test.ts
+++ src/redux/reducers/todoListReducer.test.ts
@@ -22,3 +22,25 @@ test('todoListReducer should remove TodoList', ()=>{
     const endState = todoListReducer(startState, removeTodoListAC(todo1))
     expect(endState.length).toEqual(1)
 })
+test('todoListReducer should add TodoList', ()=>{
+    const startState: TodoListsStateType = [
+        {id: '1', title: "What to Learn", filter: "All"},
+        {id: '2', title: "What to Play", filter: "Completed"},
+    ]
+    const newItem = 'What to watch';
+    const endState = todoListReducer(startState, addTodoListAC(newItem))
+    expect(endState.length).toEqual(startState.length+1)
+    expect(endState[0].title).toBe(newItem)
+
+})
+test('todoListReducer change correct todoList title', ()=>{
+    const startState: TodoListsStateType = [
+        {id: '1', title: "What to Learn", filter: "All"},
+        {id: '2', title: "What to Play", filter: "Completed"},
+    ]
+    const newItem = 'New Title for todolist';
+    let todoID = startState[0].id
+    const endState = todoListReducer(startState, changeTodoListTitleAC(todoID, newItem))
+    expect(endState[0].title).toBe(newItem)
+
+})
