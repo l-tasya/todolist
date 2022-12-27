b17d9511ee54cf417973e06e6d177c5db0ebd20f
l-tasya <94777517+l-tasya@users.noreply.github.com>
2022-12-28 03:07:49 +0600

    space

--- src/redux/reducers/tasksReducer.ts
+++ src/redux/reducers/tasksReducer.ts
@@ -1,5 +1,5 @@
 import {v1} from "uuid";
-import {addTodoListAC, removeTodoListAC, todoList1, todoList2} from "./todoListReducer";
+import {addTodoListAC, removeTodoListAC} from "./todoListReducer";
 //tasks
 export type TaskType = {
     id: string
@@ -15,57 +15,59 @@ export type TasksStateType = AllTasksType
 type StateType = TasksStateType
 
 const initialState: StateType = {
-    [todoList1]: [
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "Redux", isDone: true},
-        {id: v1(), title: "React", isDone: true},
-        {id: v1(), title: "Promises", isDone: true},
-        {id: v1(), title: "Thunk", isDone: true},
-        {id: v1(), title: "GraphQL", isDone: true},
-    ],
-    [todoList2]: [
-        {id: v1(), title: "Metro", isDone: false},
-        {id: v1(), title: "Death Stranding", isDone: true},
-    ]
+    // [todoList1]: [
+    //     {id: v1(), title: "JS", isDone: true},
+    //     {id: v1(), title: "Redux", isDone: true},
+    //     {id: v1(), title: "React", isDone: true},
+    //     {id: v1(), title: "Promises", isDone: true},
+    //     {id: v1(), title: "Thunk", isDone: true},
+    //     {id: v1(), title: "GraphQL", isDone: true},
+    // ],
+    // [todoList2]: [
+    //     {id: v1(), title: "Metro", isDone: false},
+    //     {id: v1(), title: "Death Stranding", isDone: true},
+    // ]
 }
 
 export const tasksReducer = (state: StateType = initialState, action: ActionsType): StateType => {
     switch (action.type) {
-        case "REMOVE-TASK":{
+        case "REMOVE-TASK": {
             const stateCopy = {...state}
-            stateCopy[action.todoListID] = stateCopy[action.todoListID].filter(t=>t.id !== action.taskID)
+            stateCopy[action.todoListID] = stateCopy[action.todoListID].filter(t => t.id !== action.taskID)
             return stateCopy
         }
-        case "CHANGE-CHECKBOX":{
+        case "CHANGE-CHECKBOX": {
             const stateCopy = {...state}
-            let task = stateCopy[action.todoListID].find(t=>t.id === action.taskID)
-            if(task){
+            let task = stateCopy[action.todoListID].find(t => t.id === action.taskID)
+            if (task) {
                 task.isDone = action.newValue
             }
+            stateCopy[action.todoListID] = [...stateCopy[action.todoListID]]
             return stateCopy
         }
-        case "ADD-TASK":{
+        case "ADD-TASK": {
             const stateCopy = {...state}
             const newItem: TaskType = {id: v1(), title: action.newValue, isDone: false}
             stateCopy[action.todoListID] = [newItem, ...stateCopy[action.todoListID]]
             return stateCopy
         }
-        case "ADD-TODOLIST":{
+        case "ADD-TODOLIST": {
             const stateCopy = {...state}
             stateCopy[action.id] = []
             return stateCopy
         }
-        case "REMOVE-TODOLIST":{
+        case "REMOVE-TODOLIST": {
             const stateCopy = {...state}
             delete stateCopy[action.todoListID]
             return stateCopy
         }
-        case "CHANGE-TASK-TITLE":{
+        case "CHANGE-TASK-TITLE": {
             const stateCopy = {...state}
-            const task = stateCopy[action.todoListID].find(t=>t.id === action.taskID)
-            if(task){
+            const task = stateCopy[action.todoListID].find(t => t.id === action.taskID)
+            if (task) {
                 task.title = action.newValue
             }
+            stateCopy[action.todoListID] = [...stateCopy[action.todoListID]]
             return stateCopy
         }
         default: {
@@ -75,38 +77,38 @@ export const tasksReducer = (state: StateType = initialState, action: ActionsTyp
 }
 
 type ActionsType = ReturnType<typeof removeTaskAC>
-|ReturnType<typeof changeCheckBoxAC>
-|ReturnType<typeof addTaskAC>
-|ReturnType<typeof addTodoListAC>
-|ReturnType<typeof removeTodoListAC>
-|ReturnType<typeof changeTaskTitleAC>
+    | ReturnType<typeof changeCheckBoxAC>
+    | ReturnType<typeof addTaskAC>
+    | ReturnType<typeof addTodoListAC>
+    | ReturnType<typeof removeTodoListAC>
+    | ReturnType<typeof changeTaskTitleAC>
 
-export const removeTaskAC = (todoListID: string, taskID: string) =>{
+export const removeTaskAC = (todoListID: string, taskID: string) => {
     return {
-        type: 'REMOVE-TASK',
+        type: "REMOVE-TASK",
         todoListID,
         taskID
     } as const
 }
-export const changeCheckBoxAC = (todoListID: string, taskID: string, newValue: boolean) =>{
+export const changeCheckBoxAC = (todoListID: string, taskID: string, newValue: boolean) => {
     return {
-        type: 'CHANGE-CHECKBOX',
+        type: "CHANGE-CHECKBOX",
         todoListID,
         taskID,
         newValue
     } as const
 }
 
-export const addTaskAC = (todoListID: string, newValue: string) =>{
+export const addTaskAC = (todoListID: string, newValue: string) => {
     return {
-        type: 'ADD-TASK',
+        type: "ADD-TASK",
         todoListID,
         newValue
     } as const
 }
-export const changeTaskTitleAC = (todoListID: string, taskID: string, newValue: string) =>{
+export const changeTaskTitleAC = (todoListID: string, taskID: string, newValue: string) => {
     return {
-        type: 'CHANGE-TASK-TITLE',
+        type: "CHANGE-TASK-TITLE",
         todoListID,
         taskID,
         newValue
