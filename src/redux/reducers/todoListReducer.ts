1be0f533f0a4c4b319873e31975c199ac998527d
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-04 22:30:48 +0600

    moved types to a separate file

--- src/redux/reducers/todoListReducer.ts
+++ src/redux/reducers/todoListReducer.ts
@@ -10,24 +10,9 @@ const initialState: ITodoListDomain[] = []
 
 
 
-export type FilterType = "All" | "Completed" | "Active"
-export type TodoListType = {
-    id: string
-    title: string
-    filter: FilterType
-}
-export type TodoListsType = TodoListType[]
-export type TodoListsStateType = TodoListsType
-type StateType = TodoListsType
 
-export const todoList1 = v1()
-export const todoList2 = v1()
-const initialState: StateType = [
-    {id: todoList1, title: "What to Learn", filter: "All"},
-    {id: todoList2, title: "What to Play", filter: "Completed"},
-]
-//todo: read about uuid and fix simple ids for todo and task
-export const todoListReducer = (state: StateType = initialState, action: ActionsType): StateType => {
+
+export const todoListReducer = (state: ITodoListDomain[] = initialState, action: ActionsType): ITodoListDomain[] => {
     switch (action.type) {
         case "SET-FILTER":{
             const stateCopy = [...state]
