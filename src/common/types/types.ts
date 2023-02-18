2c36e5fd99c04492293ed6825e91c4a914dfa4c4
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:16:13 +0600

    adapted global types to API returning types

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,33 +1,63 @@
+import { RequestStatusType } from "../../redux/reducers/app-reducer"
+
 export type ITodoList = {
     id: string
-    addedDate?: typeof Date
+    addedDate: string
     order: number
     title: string
 
 }
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
 export type ITask = {
-    completed: boolean
-    title: string
     description: string
-    status: number
-    priority: number
-    startDate: typeof Date
-    deadline: typeof Date
-    todoListId: string
+    title: string
+    status: TaskStatuses
+    priority: TaskPriorities
+    startDate: string
+    deadline: string
     id: string
+    todoListId: string
     order: number
-    addedDate: typeof Date
+    addedDate: string
 }
 export type ITodoListDomain = ITodoList & {
     filter: FilterType
+    entityStatus: RequestStatusType
 }
 export type FilterType = "All" | "Completed" | "Active"
 
 
 
-
+export enum ResultCodes {
+    Success = 0,
+    Error = 1,
+    Captcha = 10,
+}
 
 export type TasksReducerType = {
-    [key: string]: ITask[]
+    [key: string]: ITaskDomain[]
+}
+export type ITaskDomain = ITask &{
+    entityStatus: RequestStatusType
 }
 export type TodoListReducerType = ITodoListDomain[]
+
+
+
+export type ErrorCustomType = {
+    messages: string[]
+    fieldError: string
+}
