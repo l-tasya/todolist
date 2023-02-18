6612abee3f67efd09a1f4e8c3711ab9978015179
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:05:09 +0600

    adjusted the values ​​to the actual types

--- src/redux/reducers/tasksReducer.test.ts
+++ src/redux/reducers/tasksReducer.test.ts
@@ -1,96 +1,104 @@
-import {v1} from "uuid";
-import {changeCheckBoxAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./tasksReducer";
-import {TasksReducerType} from "../../common/types/types";
+import {v1} from 'uuid';
+import {
+    changeTaskStatusAC,
+    changeTaskTitleAC,
+    removeTaskAC,
+    setTaskEntity,
+    setTasksAC,
+    tasksReducer
+} from './tasksReducer';
+import {ITaskDomain, TasksReducerType, TaskStatuses} from '../../common/types/types';
+import {RequestStatusType} from './app-reducer';
 
 const startState: TasksReducerType = {
-    ["1"]: [
+    ['1']: [
         {
             id: v1(),
-            title: "JS",
-            completed: false,
-            addedDate: Date,
-            deadline: Date,
-            description: "",
+            title: 'JS',
+            addedDate: '',
+            deadline: '',
+            description: '',
             order: 0,
             priority: 0,
-            startDate: Date,
+            startDate: '',
             status: 0,
-            todoListId: '1'
+            todoListId: '1',
+            entityStatus: 'idle'
         },
         {
             id: v1(),
-            title: "Redux",
-            completed: false,
-            addedDate: Date,
-            deadline: Date,
-            description: "",
+            title: 'Redux',
+            addedDate: '',
+            deadline: '',
+            description: '',
             order: 0,
             priority: 0,
-            startDate: Date,
+            startDate: '',
             status: 0,
-            todoListId: '1'
+            todoListId: '1',
+            entityStatus: 'idle'
         },
         {
             id: v1(),
-            title: "React",
-            completed: false,
-            addedDate: Date,
-            deadline: Date,
-            description: "",
+            title: 'React',
+            addedDate: '',
+            deadline: '',
+            description: '',
             order: 0,
             priority: 0,
-            startDate: Date,
+            startDate: '',
             status: 0,
-            todoListId: '1'
+            todoListId: '1',
+            entityStatus: 'idle'
         },
     ],
-    ["2"]: [
+    ['2']: [
         {
             id: v1(),
-            title: "JS",
-            completed: false,
-            addedDate: Date,
-            deadline: Date,
-            description: "",
+            title: 'JS',
+            addedDate: '',
+            deadline: '',
+            description: '',
             order: 0,
             priority: 0,
-            startDate: Date,
+            startDate: '',
             status: 0,
-            todoListId: '1'
+            todoListId: '1',
+            entityStatus: 'idle'
         },
         {
             id: v1(),
-            title: "Redux",
-            completed: false,
-            addedDate: Date,
-            deadline: Date,
-            description: "",
+            title: 'Redux',
+            addedDate: '',
+            deadline: '',
+            description: '',
             order: 0,
             priority: 0,
-            startDate: Date,
+            startDate: '',
             status: 0,
-            todoListId: '1'
+            todoListId: '1',
+            entityStatus: 'idle'
         },
     ]
 }
-test("tasksReducer should remove correct task", () => {
+test('tasksReducer should remove correct task', () => {
     let todoListID = Object.keys(startState)[0]
     let taskID = startState[todoListID][0].id
     const endState = tasksReducer(startState, removeTaskAC(todoListID, taskID))
 
     expect(endState[todoListID].length).toBe(startState[todoListID].length - 1)
 })
-test("tasksReducer should change checkbox", () => {
+test('tasksReducer should change status', () => {
     let todoListID = Object.keys(startState)[0]
     let taskID = startState[todoListID][0].id
-    let newValue = false
-    const endState = tasksReducer(startState, changeCheckBoxAC(todoListID, taskID, newValue))
-    expect(endState[todoListID][0].completed).toBe(newValue)
+    let newValue = TaskStatuses.Completed
+    const endState = tasksReducer(startState, changeTaskStatusAC(todoListID, taskID, newValue))
+    expect(endState[todoListID][0].status).toBe(newValue)
 })
-test("tasksReducer should title of correct task", () => {
+test('tasksReducer should change title of correct task', () => {
     let todoListID = Object.keys(startState)[0]
     let taskID = startState[todoListID][0].id
-    let newValue = "New Item"
+    let newValue = 'New Item'
     const endState = tasksReducer(startState, changeTaskTitleAC(todoListID, taskID, newValue))
     expect(endState[todoListID].find(t => t.id === taskID)?.title).toBe(newValue)
 })
