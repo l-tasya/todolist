46fc23ddb7dff97df98883c4cdb5395b262dfb79
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-18 23:55:19 +0600

    added snackbar who alert errors

--- src/App.tsx
+++ src/App.tsx
@@ -18,5 +18,6 @@ export const App = () => {
             <LinearProgress variant={'determinate'} value={100} color={'error'} sx={stylesForLoading}/>}
             <TodoListsLists/>
         </Content>
+add        <ErrorSnackbar/>
     </Wrapper>
 }
