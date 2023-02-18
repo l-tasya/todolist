0e2b75c3da07755249cf1d84931341c273331249
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:11:04 +0600

    adjusted the values ​​to the actual types

--- src/redux/reducers/todolist-tasks-reducer.test.ts
+++ src/redux/reducers/todolist-tasks-reducer.test.ts
@@ -1,20 +1,19 @@
 import {tasksReducer} from "./tasksReducer";
 import {addTodoListAC, todoListReducer} from "./todoListReducer";
-import {TasksReducerType, TodoListReducerType} from "../../common/types/types";
+import {ITodoList, TasksReducerType, TodoListReducerType} from "../../common/types/types";
+
+
+const startTasksState: TasksReducerType = {}
+const startTodoListsState: TodoListReducerType = []
 
 test("ids should be equals", () => {
-    const startTasksState: TasksReducerType = {}
-    const startTodoListsState: TodoListReducerType = []
-
-    const action = addTodoListAC("no matter")
+    let newTODO = {id: 'd', addedDate: '', order: 0, title: 'test'}
+    const action = addTodoListAC(newTODO)
     const endTasksState = tasksReducer(startTasksState, action)
     const endTodoListsState = todoListReducer(startTodoListsState, action)
-
     const keys = Object.keys(endTasksState)
     const idFromTasks = keys[0]
     const idFromTodoLists = endTodoListsState[0].id
-
-
-    expect(idFromTasks).toBe(action.id)
-    expect(idFromTodoLists).toBe(action.id)
+    expect(idFromTasks).toBe(newTODO.id)
+    expect(idFromTodoLists).toBe(newTODO.id)
 })
