d90cd16c2bb2f78e753d8a4730b408ec6b704811
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:04:38 +0600

    added disable when task is updating\deleting\renaming

--- src/components/TodoList/Task/Task.tsx
+++ src/components/TodoList/Task/Task.tsx
@@ -1,32 +1,36 @@
-import React, {ChangeEvent, useCallback} from "react";
-import {EditableSpan} from "../../../common/components/EditableSpan/EditableSpan";
-import {RemoveItem} from "../../../common/components/RemoveC/RemoveItem";
-import {Container} from "./styles";
+import React, {ChangeEvent, useCallback} from 'react';
+import {EditableSpan} from '../../../common/components/EditableSpan/EditableSpan';
+import {RemoveItem} from '../../../common/components/RemoveC/RemoveItem';
+import {Container} from './styles';
+import {ITask, TaskStatuses} from '../../../common/types/types';
+import {RequestStatusType} from '../../../redux/reducers/app-reducer';
 
 interface IProps {
-    changeCheckBox: (todoID: string, taskID: string, newValue: boolean) => void
+    changeStatus: (todoID: string, taskID: string, status: TaskStatuses) => void
     changeTaskTitle: (todoID: string, taskID: string, value: string) => void
     removeTask: (todoID: string, id: string) => void
     todoID: string
+    entity: RequestStatusType
     //task
-    id: string
-    isDone: boolean
-    title: string
+    task: ITask
 
 }
 
-export const Task: React.FC<IProps> = React.memo(({changeCheckBox, removeTask, changeTaskTitle, id, todoID, isDone, title}) => {
-        const suicide = useCallback(() => removeTask(todoID, id), [removeTask, id, todoID])
-        const changeStatus = useCallback((e: ChangeEvent<HTMLInputElement>) => changeCheckBox(todoID, id, e.currentTarget.checked), [changeCheckBox, id, todoID])
-        const changeTitle = useCallback((value: string) => changeTaskTitle(todoID, id, value), [id, todoID, changeTaskTitle])
+export const Task: React.FC<IProps> = React.memo(({changeStatus, removeTask, changeTaskTitle, todoID, task, entity}) => {
+        const suicide = useCallback(() => removeTask(todoID, task.id), [removeTask, task.id, todoID])
+        const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+            changeStatus(todoID, task.id, e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New)
+        }, [changeStatus, task.id, todoID])
+        const changeTitle = useCallback((value: string) => changeTaskTitle(todoID, task.id, value), [task.id, todoID, changeTaskTitle])
 
         return <Container>
-            <input type={"checkbox"}
-                   onChange={changeStatus}
-                   checked={isDone}
+            <input type={'checkbox'}
+                   onChange={onChangeHandler}
+                   checked={task.status === TaskStatuses.Completed}
+                   disabled={entity === 'loading'}
             />
-            <EditableSpan c1={changeTitle} title={title}/>
-            <RemoveItem removeCallback={suicide}/>
+            <EditableSpan disabled={entity === 'loading'} c1={changeTitle} title={task.title}/>
+            <RemoveItem disabled={entity === 'loading'} removeCallback={suicide}/>
         </Container>
     }
 )
