64008d991ff56ea2b8e55115275506046c4b78a8
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:10:18 +0600

    adjusted the values ​​to the actual types

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -13,21 +13,26 @@ import {RequestStatusType} from '../../redux/reducers/app-reducer';
 interface IProps {
     id: string
     title: string
-    tasks: ITask[]
+    tasks: ITaskDomain[]
     filter: FilterType
+    entityStatus: RequestStatusType
     setFilter: (todoListID: string, newValue: FilterType) => void
     removeTask: (todoListsID: string, taskID: string) => void
     removeTodoList: (todoListsID: string) => void
-    changeCheckBox: (todoListID: string, taskID: string, newValue: boolean) => void
+    changeStatus: (todoListID: string, taskID: string, status: TaskStatuses) => void
     changeTodoListTitle: (todoListID: string, newTitle: string) => void
     changeTaskTitle: (todoListID: string, taskID: string, newTitle: string) => void
     addTask: (todoListID: string, newValue: string) => void
 }
 
-export const TodoList: React.FC<IProps> = React.memo(({title, filter, tasks, setFilter, id, removeTask, removeTodoList, changeCheckBox, changeTodoListTitle, changeTaskTitle, addTask,}) => {
-        const addTaskCallback = useCallback((title: string) => {
-            addTask(id, title)
-        }, [id, addTask])
+export const TodoList: React.FC<IProps> = React.memo<IProps>(({title, filter, tasks, setFilter, id, removeTask, removeTodoList, changeStatus, changeTodoListTitle, changeTaskTitle, addTask, entityStatus}) => {
+        const dispatch = useAppDispatch()
+        useEffect(() => {
+            dispatch(fetchTasksTC(id))
+        }, [dispatch, id])
+
+
+        const addTaskCallback = useCallback((title: string) => addTask(id, title), [id, addTask])
         const changeTodoListTitleCallback = useCallback((title: string) => {
             changeTodoListTitle(id, title)
         }, [changeTodoListTitle, id])
@@ -37,16 +42,29 @@ export const TodoList: React.FC<IProps> = React.memo(({title, filter, tasks, set
         //filter
 
         let resultTasks = tasks;
-        if (filter === "Active") {
-            resultTasks = tasks.filter(t => !t.completed)
+        if (filter === 'Active') {
+            resultTasks = tasks.filter(t => t.status === TaskStatuses.New)
         }
-        if (filter === "Completed") {
-            resultTasks = tasks.filter(t => t.completed)
+        if (filter === 'Completed') {
+            resultTasks = tasks.filter(t => t.status === TaskStatuses.Completed)
         }
 
         const changeFilter = useCallback((e: React.MouseEvent<HTMLElement>, newValue: FilterType) => {
             setFilter(id, newValue)
         }, [setFilter, id])
+
+
+        const tasksElements = resultTasks?.map(t => {
+            return <Task
+                task={t}
+                entity={t.entityStatus}
+                key={t.id}
+                changeTaskTitle={changeTaskTitle}
+                changeStatus={changeStatus}
+                removeTask={removeTask}
+                todoID={id}
+            />
+        })
         return <Container>
             <Header>
                 <EditableSpan c1={changeTodoListTitleCallback} title={title}/>
