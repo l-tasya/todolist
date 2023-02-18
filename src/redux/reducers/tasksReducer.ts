953bff42c227cea6bd6f865899e74f3b469efe8a
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:08:51 +0600

    added error handling, and loading logic

--- src/redux/reducers/tasksReducer.ts
+++ src/redux/reducers/tasksReducer.ts
@@ -1,6 +1,10 @@
-import {v1} from "uuid";
-import {addTodoListAC, removeTodoListAC, setTodoListsAC} from "./todoListReducer";
-import {ITask, TasksReducerType} from "../../common/types/types";
+import {addTodoListAC, removeTodoListAC, setTodoListsAC} from './todoListReducer';
+import {ITask, ITaskDomain, ResultCodes, TasksReducerType, TaskStatuses} from '../../common/types/types';
+import {Dispatch} from 'redux';
+import {todoListsAPI} from '../../api/todolists-api';
+import {AppStateType} from '../store/store';
+import {RequestStatusType, setErrorAC, setLoadingStatusAC} from './app-reducer';
+import {handleServerAppError, handleServerNetworkError} from '../../common/utils/error-utils';
 
 
 type StateType = TasksReducerType
@@ -80,6 +84,10 @@ type ActionsType = ReturnType<typeof removeTaskAC>
     | ReturnType<typeof removeTodoListAC>
     | ReturnType<typeof changeTaskTitleAC>
     | ReturnType<typeof setTodoListsAC>
+    | ReturnType<typeof setTasksAC>
+    | ReturnType<typeof setLoadingStatusAC>
+    | ReturnType<typeof setErrorAC>
+    | ReturnType<typeof setTaskEntity>
 
 export const removeTaskAC = (todoListID: string, taskID: string) => {
     return {
@@ -90,13 +98,21 @@ export const removeTaskAC = (todoListID: string, taskID: string) => {
 }
 export const changeCheckBoxAC = (todoListID: string, taskID: string, newValue: boolean) => {
     return {
-        type: "CHANGE-CHECKBOX",
+        type: 'ADD-TASK',
+        task
+    } as const
+}
+
+export const changeTaskTitleAC = (todoListID: string, taskID: string, newValue: string) => {
+    return {
+        type: 'CHANGE-TASK-TITLE',
         todoListID,
         taskID,
         newValue
     } as const
 }
-export const addTaskAC = (todoListID: string, newValue: string) => {
+
+export const setTasksAC = (todoListID: string, tasks: ITask[]) => {
     return {
         type: "ADD-TASK",
         todoListID,
@@ -110,4 +126,117 @@ export const changeTaskTitleAC = (todoListID: string, taskID: string, newValue: 
         taskID,
         newValue
     } as const
+}
+
+
+//thunk
+
+export const fetchTasksTC = (todoListID: string) => {
+    return (dispatch: Dispatch<ActionsType>) => {
+        dispatch(setLoadingStatusAC('loading'))
+        todoListsAPI.getTasks(todoListID)
+            .then((res) => {
+                const tasks = res.data.items;
+                dispatch(setTasksAC(todoListID, tasks))
+                setTimeout(() => dispatch(setLoadingStatusAC('succeeded')), 300)
+            })
+    }
+}
+//
+export const removeTaskTC = (todoListID: string, taskID: string) => {
+    return async (dispatch: Dispatch<ActionsType>) => {
+        dispatch(setLoadingStatusAC('loading'))
+        dispatch(setTaskEntity(todoListID, taskID, 'loading'))
+        todoListsAPI.deleteTask(todoListID, taskID)
+            .then((res) => {
+                    if (res.data.resultCode === 0) {
+                        const action = removeTaskAC(todoListID, taskID)
+                        dispatch(action)
+                        setTimeout(() => dispatch(setLoadingStatusAC('succeeded')), 300)
+                        dispatch(setTaskEntity(todoListID, taskID, 'succeeded'))
+                    }
+
+                }
+            )
+            .catch((error) => {
+                handleServerNetworkError(dispatch, error)
+            })
+    }
+}
+//server error
+export const addTaskTC = (todoListID: string, title: string) => {
+    return (dispatch: Dispatch<ActionsType>) => {
+        dispatch(setLoadingStatusAC('loading'))
+        todoListsAPI.createTask(todoListID, title)
+            .then((res) => {
+                if (res.data.resultCode === ResultCodes.Success) {
+                    dispatch(addTaskAC(res.data.data.item))
+                    setTimeout(() => dispatch(setLoadingStatusAC('succeeded')), 300)
+                } else {
+                    handleServerAppError(res.data, dispatch)
+                }
+            })
+            .catch((error) => {
+                handleServerNetworkError(dispatch, error)
+            })
+    }
+}
+//default
+export const updateTaskStatusTC = (todoListID: string, taskID: string, status: TaskStatuses) => {
+    return (dispatch: Dispatch<ActionsType>, getState: () => AppStateType) => {
+        dispatch(setLoadingStatusAC('loading'))
+        const allTasksFromState = getState().tasks;
+        const tasksForCurrentTodoList = allTasksFromState[todoListID]
+        const task = tasksForCurrentTodoList.find(t => t.id === taskID)
+        if (task) {
+            todoListsAPI.updateTask(todoListID, taskID, {
+                title: task.title,
+                startDate: task.startDate,
+                priority: task.priority,
+                description: task.description,
+                deadline: task.deadline,
+                status: status,
+            }).then(() => {
+                const action = changeTaskStatusAC(todoListID, taskID, status)
+                dispatch(action)
+                setTimeout(() => dispatch(setLoadingStatusAC('succeeded')), 300)
+            })
+                .catch((error) => {
+                    handleServerNetworkError(dispatch, error)
+                })
+        }
+    }
+}
+//default
+export const updateTaskTitleTC = (todoListID: string, taskID: string, title: string) => {
+
+    return (dispatch: Dispatch<ActionsType>, getState: () => AppStateType) => {
+        dispatch(setLoadingStatusAC('loading'))
+        const allTasksFromState = getState().tasks
+        const tasksForCurrentTodoList = allTasksFromState[todoListID]
+        const task = tasksForCurrentTodoList.find(t => t.id === taskID)
+        if (task) {
+            todoListsAPI.updateTask(todoListID, taskID, {
+                title: title,
+                startDate: task.startDate,
+                priority: task.priority,
+                description: task.description,
+                deadline: task.deadline,
+                status: task.status,
+            })
+                .then((res) => {
+                    if (res.data.resultCode === ResultCodes.Success) {
+                        const action = changeTaskTitleAC(todoListID, taskID, title)
+                        dispatch(action)
+                        setTimeout(() => dispatch(setLoadingStatusAC('succeeded')), 300)
+                    } else {
+                        handleServerAppError(res.data, dispatch)
+                    }
+
+                })
+                .catch((e) => {
+                    handleServerNetworkError(dispatch, e)
+                })
+        }
+    }
 }
