8c494172d129f92dd8bbc277cf2b333e1639a1e7
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:13:57 +0600

    adjusted the values ​​to the actual types

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosResponse} from "axios";
-import {ITask, ITodoList} from "../common/types/types";
+import {ITask, ITodoList, ResultCodes, TaskPriorities, TaskStatuses} from '../common/types/types';
 
 
 const settings = {
@@ -21,7 +21,7 @@ export const todoListsAPI = {
         return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: ITodoList }>>>("todo-lists", {title: title});
     },
     deleteTodoList: (todoListID: string) => {
-        return instance.delete<ResponseType>(`todo-list/${todoListID}`);
+        return instance.delete<ResponseType>(`todo-lists/${todoListID}`);
     },
     updateTodoList: (todoListID: string, title: string) => {
         return instance
@@ -34,17 +34,17 @@ export const todoListsAPI = {
         return instance.get<GetTasksResponseType>(`todo-lists/${todoListID}/tasks`);
     },
     createTask: (todoListID: string, title: string) => {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType>>(`todo-lists/${todoListID}/tasks`, {title: title})
+        return instance.post<{ title: string }, AxiosResponse<ResponseType<{item: ITask}>>>(`todo-lists/${todoListID}/tasks`, {title: title})
     },
     deleteTask: (todoListID: string, taskID: string) => {
         return instance.delete<ResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`)
     },
-    updateTask: (todoListID: string, taskID: string, title: string) => {
-        return instance.put<ResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`, {title: title})
+    updateTask: (todoListID: string, taskID: string, model: UpdateTaskModelType) => {
+        return instance.put<UpdateTaskModelType, AxiosResponse<ResponseType<{ item: ITask }>>>(`todo-lists/${todoListID}/tasks/${taskID}`, model)
     }
 }
-type ResponseType<D = {}> = {
-    resultCode: number
+export type ResponseType<D = {}> = {
+    resultCode: ResultCodes
     messages: Array<string>
     data: D
     fieldsErrors: string[]
@@ -53,4 +53,13 @@ type GetTasksResponseType = {
     items: ITask[]
     totalCount: number
     error: string
+}
+
+export type UpdateTaskModelType = {
+    title: string
+    description: string
+    status: TaskStatuses
+    priority: TaskPriorities
+    startDate: string
+    deadline: string
 }
