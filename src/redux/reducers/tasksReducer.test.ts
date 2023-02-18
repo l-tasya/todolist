0186d5ef274a50c63eb9fe94dc492c2e6ad25557
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-19 00:05:39 +0600

    added tests(setTasks, setTaskEntity)

--- src/redux/reducers/tasksReducer.test.ts
+++ src/redux/reducers/tasksReducer.test.ts
@@ -102,3 +102,45 @@ test('tasksReducer should change title of correct task', () => {
     const endState = tasksReducer(startState, changeTaskTitleAC(todoListID, taskID, newValue))
     expect(endState[todoListID].find(t => t.id === taskID)?.title).toBe(newValue)
 })
+test('tasksReducer should set tasks', () => {
+    let todoListID = Object.keys(startState)[0]
+    let newValue: ITaskDomain[] = [
+        {
+            id: '1',
+            title: 'React',
+            todoListId: 'todoID1',
+            status: 0,
+            startDate: '',
+            addedDate: '',
+            deadline: '',
+            priority: 0,
+            order: 0,
+            description: '',
+            entityStatus: 'idle'
+        },
+        {
+            id: '2',
+            title: 'Redux',
+            todoListId: 'todoID2',
+            status: 0,
+            startDate: '',
+            addedDate: '',
+            deadline: '',
+            priority: 0,
+            order: 0,
+            description: '',
+            entityStatus: 'idle'
+        }
+    ]
+    const endState = tasksReducer(startState, setTasksAC(todoListID, newValue))
+    expect(endState[todoListID].length).toBe(newValue.length)
+    expect(endState[todoListID][0]).toBe(newValue[0])
+})
+test('tasksReducer should change correct task entity', ()=>{
+    const newValue: RequestStatusType = 'loading'
+    let todoID = Object.keys(startState)[0]
+    let taskID = startState[todoID][0].id
+
+    const endState = tasksReducer(startState, setTaskEntity(todoID, taskID, newValue))
+    expect(endState[todoID][0].entityStatus).toEqual(newValue)
+})
