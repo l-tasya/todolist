beba2986fb4f2ecea78dc854ffb51ae03d095b6a
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-02-27 21:54:08 +0600

    added some styles

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,13 +4,15 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
-import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from 'formik';
 import {loginTC} from '../../redux/reducers/auth-reducer';
 import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
 import {Navigate} from 'react-router-dom';
+import Paper from '@mui/material/Paper/Paper';
+import styled from 'styled-components';
+import {Title} from '../../common/styles/global';
 
 
 type FormikErrorType = {
@@ -38,6 +40,26 @@ type Initial = {
     password: string,
     rememberMe: boolean
 }
+const Container = styled(Paper)`
+    width: 300px;
+    padding: 8px;
+`
+const SubTitle = styled(Title)`
+  font-size: 10px;
+  text-align:left;
+  color: gray;
+    span{
+    font-size: 8px;
+    font-family: MyFont,serif;
+    color: white;
+    background: #0000cd;
+    padding: 2px;
+    }
+    div{
+    font-size: 12px;
+    }
+    
+`
 export const Login: React.FC = () => {
     const dispatch = useAppDispatch()
     const isLoggedIn = useAppSelector(t => t.auth.isLoggedIn)
@@ -55,55 +77,55 @@ export const Login: React.FC = () => {
         },
     })
     if (isLoggedIn) {
-      return <Navigate to={"/todolist/"}/>
+        return <Navigate to={'/todolist/'}/>
     }
 
+    return <Container>
+        <Title style={{fontSize: '20px'}}>Login</Title>
+        <Grid container justifyContent={'center'}>
+            <Grid item justifyContent={'center'}>
+                <FormControl>
+                    <SubTitle>
+                        <div>To log in get registered
+                            <a href="https://social-network.samuraijs.com/"
+                               target="_blank"
+                               rel="noreferrer"> here
+                            </a> or use common test account credentials:
+                        </div>
+                        <p><span>Email</span>: free@samuraijs.com</p>
+                        <p><span>Password</span>: free</p>
+                    </SubTitle>
+                    <form onSubmit={formik.handleSubmit}>
+                        <FormGroup>
 
+                            <TextField
+                                label="Email"
+                                margin="normal"
+                                error={formik.touched.email && Boolean(formik.errors.email)}
+                                helperText={formik.touched.email && formik.errors.email}
+                                {...formik.getFieldProps('email')}
+                            />
 
-    return <Grid container justifyContent={'center'}>
-        <Grid item justifyContent={'center'}>
-            <FormControl>
-                <FormLabel>
-                    <p>To log in get registered
-                        <a href="https://social-network.samuraijs.com/"
-                           target="_blank"
-                           rel="noreferrer"> here
-                        </a>
-                    </p>
-                    <p>or use common test account credentials:</p>
-                    <p>Email: free@samuraijs.com</p>
-                    <p>Password: free</p>
-                </FormLabel>
-                <form onSubmit={formik.handleSubmit}>
-                    <FormGroup>
+                            <TextField
+                                type="password"
+                                label="Password"
+                                margin="normal"
+                                error={formik.touched.password && Boolean(formik.errors.password)}
+                                helperText={formik.touched.password && formik.errors.password}
+                                {...formik.getFieldProps('password')}
+                            />
 
-                        <TextField
-                            label="Email"
-                            margin="normal"
-                            error={formik.touched.email && Boolean(formik.errors.email)}
-                            helperText={formik.touched.email && formik.errors.email}
-                            {...formik.getFieldProps('email')}
-                        />
-
-                        <TextField
-                            type="password"
-                            label="Password"
-                            margin="normal"
-                            error={formik.touched.password && Boolean(formik.errors.password)}
-                            helperText={formik.touched.password && formik.errors.password}
-                            {...formik.getFieldProps('password')}
-                        />
-
-                        <FormControlLabel label={'Remember me'}
-                                          control={<Checkbox
-                                              checked={formik.values.rememberMe}
-                                              {...formik.getFieldProps('rememberMe')}/>}/>
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>
-                            Login
-                        </Button>
-                    </FormGroup>
-                </form>
-            </FormControl>
+                            <FormControlLabel label={'Remember me'}
+                                              control={<Checkbox
+                                                  checked={formik.values.rememberMe}
+                                                  {...formik.getFieldProps('rememberMe')}/>}/>
+                            <Button type={'submit'} variant={'contained'} color={'primary'}>
+                                Login
+                            </Button>
+                        </FormGroup>
+                    </form>
+                </FormControl>
+            </Grid>
         </Grid>
-    </Grid>
+    </Container>
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,15 @@
-import React from "react"
-import styled from "styled-components";
-import {NavBarContainer, Title} from "../../common/styles/global";
-
-
+import React from 'react'
+import styled from 'styled-components';
+import {NavBarContainer, Title} from '../../common/styles/global';
+import {useAppDispatch, useAppSelector} from '../../common/hooks/hooks';
+import {logoutTC} from '../../redux/reducers/auth-reducer';
+import Grid from '@mui/material/Grid/Grid';
+import Button from '@mui/material/Button/Button';
 
 
 const Container = styled(NavBarContainer)`
-
+  justify-content: space-between;
+  padding: 8px;
 `
 const TitleC = styled(Title)`
 font-family: MyFont,serif;
@@ -15,14 +18,38 @@ color: white;
 text-align: center;
 margin: 15px;
 `
-const SubTitle = styled(TitleC)`
+export const SubTitle = styled(TitleC)`
     font-size: 14px;
     margin: 0
 `
-interface IProps {}
+const Status = styled.div`
+width: 10px;
+height: 10px;
+border: 1px solid white;
+border-radius: 50%;
+margin-left: 5px;
+transition: 1s;
+`
+
+interface IProps {
+}
+
 export const NavBar: React.FC<IProps> = React.memo(() => {
+
+    let value = useAppSelector(t => t.auth.isLoggedIn)
+    const style = {
+        background: value ? '#62d74d' : '#f16868',
+
+    }
+    const dispatch = useAppDispatch()
     return <Container>
-        <TitleC>TODO-LIST</TitleC>
-        <SubTitle>l-tasya</SubTitle>
+        <div>
+            <TitleC>TODO-LIST</TitleC>
+            <Grid container alignItems={'center'} direction={'row'} justifyContent="center"><SubTitle>l-tasya</SubTitle><Status
+                style={style}/></Grid>
+        </div>
+        <Button size={'small'} onClick={() => dispatch(logoutTC())}>
+            Log Out
+        </Button>
     </Container>
 })
