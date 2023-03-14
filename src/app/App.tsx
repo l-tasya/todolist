ebf0dc6f60357f29f4f5480f9728f43afed93d8e
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-03-14 23:33:09 +0600

    deploy adjusting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -35,8 +35,8 @@ export const App = () => {
                 <LinearProgress variant={'determinate'} value={100} color={'error'} sx={stylesForLoading}/>}
             <div>{isInitialized}</div>
             <Routes>
-                <Route path={'/'} element={<TodoListsList/>}/>
-                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/todolist/'} element={<TodoListsList/>}/>
+                <Route path={'/todolist/login'} element={<Login/>}/>
                 <Route path={'/404'} element={<h1>Page Not Found. 404</h1>}/>
                 <Route path={'*'} element={<Navigate to={'/404'}/>}/>
             </Routes>
