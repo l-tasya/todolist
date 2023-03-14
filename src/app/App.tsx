afe7efec7514fa90fab34b231e4262c4f9f49418
l-tasya <94777517+l-tasya@users.noreply.github.com>
2023-03-14 23:23:45 +0600

    deploy adjusting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,14 @@
 import React, {useEffect} from 'react';
 
-import {Content, stylesForLoading, Wrapper} from '../common/styles/global';
-import {NavBar} from '../components/NavBar/NavBar';
+import {Wrapper} from '../common/styles/global';
 import {useAppSelector} from '../common/hooks/hooks';
-import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
-import LinearProgress from '@mui/material/LinearProgress/LinearProgress';
 import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
 import {useActions} from "../utils/redux-utils";
 import {appActions} from '../features/Application';
-import {Navigate, Route, Routes} from 'react-router-dom';
-import {Login} from '../components/Login/Login';
-import {TodoListsList} from "../features/TodoListLists/TodoListsList";
 
 
 export const App = () => {
-    const status = useAppSelector(t => t.app.status)
+    // const status = useAppSelector(t => t.app.status)
     const {initializeAppTC} = useActions(appActions)
     const isInitialized = useAppSelector(t => t.app.isInitialized)
     useEffect(() => {
@@ -28,19 +22,20 @@ export const App = () => {
     }
 
     return <Wrapper>
-        <NavBar/>
-        <Content>
-            {status === 'loading' && <LinearProgress sx={stylesForLoading}/>}
-            {status === 'failed' &&
-                <LinearProgress variant={'determinate'} value={100} color={'error'} sx={stylesForLoading}/>}
-            <div>{isInitialized}</div>
-            <Routes>
-                <Route path={'/todolist/'} element={<TodoListsList/>}/>
-                <Route path={'/todolist/login'} element={<Login/>}/>
-                <Route path={'/404'} element={<h1>Page Not Found. 404</h1>}/>
-                <Route path={'*'} element={<Navigate to={'/404'}/>}/>
-            </Routes>
-        </Content>
-        <ErrorSnackbar/>
+        sss
+        {/*<NavBar/>*/}
+        {/*<Content>*/}
+        {/*    {status === 'loading' && <LinearProgress sx={stylesForLoading}/>}*/}
+        {/*    {status === 'failed' &&*/}
+        {/*        <LinearProgress variant={'determinate'} value={100} color={'error'} sx={stylesForLoading}/>}*/}
+        {/*    <div>{isInitialized}</div>*/}
+        {/*    <Routes>*/}
+        {/*        <Route path={'/todolist/'} element={<TodoListsList/>}/>*/}
+        {/*        <Route path={'/todolist/login'} element={<Login/>}/>*/}
+        {/*        <Route path={'/404'} element={<h1>Page Not Found. 404</h1>}/>*/}
+        {/*        <Route path={'*'} element={<Navigate to={'/404'}/>}/>*/}
+        {/*    </Routes>*/}
+        {/*</Content>*/}
+        {/*<ErrorSnackbar/>*/}
     </Wrapper>
 }
